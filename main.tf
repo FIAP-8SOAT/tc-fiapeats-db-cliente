f9053fed9d3cf400eb6050ee8ef9b33c53d3200e
Vithor Amorim Escames <vithorescames@hotmail.com>
2025-02-11 22:55:33 -0300

    fix: correção do nome do db

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-backend-fiapeats" # Substitua pelo nome do bucket
-    key            = "state/fiapeatsdb-cliente/terraform.tfstate"         # Caminho do estado no bucket
+    key            = "state/fiapeats-cliente-db/terraform.tfstate"         # Caminho do estado no bucket
     region         = "us-east-1"                       # Região do bucket
     encrypt        = true                              # Criptografia no bucket
   }
@@ -17,8 +17,8 @@ data "aws_vpc" "default" {
 
 resource "aws_security_group" "default" {
     vpc_id      = "${data.aws_vpc.default.id}"
-    name        = "fiapeatsdb-cliente-sg"
-    description = "Allow all inbound for FiapeatsDB-cliente"
+    name        = "fiapeats-cliente-db-sg"
+    description = "Allow all inbound for fiapeats-cliente-db"
     ingress {
         from_port   = 5432
         to_port     = 5432
@@ -28,14 +28,14 @@ resource "aws_security_group" "default" {
 }
 
 resource "aws_db_instance" "default" {
-    identifier           = "fiapeatsdb-cliente"
+    identifier           = "fiapeats-cliente-db"
     allocated_storage    = 5
-    db_name              = "fiapeatsdb-cliente"
+    db_name              = "fiapeats-cliente-db"
     engine               = "postgres"
     engine_version       = "16.3"
     instance_class       = "db.t3.micro"
     username             = "sa"
-    password             = "fiapeats-pass"
+    password             = "fiapeats-cliente-db-pass"
     vpc_security_group_ids = [aws_security_group.default.id]
     skip_final_snapshot  = true
     publicly_accessible  = true
